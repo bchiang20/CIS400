825c06d09e14b2e55bdf4b7c70ccf479e323017b
Bryan Chiang <bry.chiang@gmail.com>
2012-04-05 12:38:12 -0400

    updated page titles

--- editThresholds.php
+++ editThresholds.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-	    <title>Senior Design - Search</title>
+	    <title>Senior Design - Edit</title>
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	    <meta name="description" content="">
 	    <meta name="author" content="">
